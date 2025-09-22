8448dfdf53560f4f3e717ddb223cfa1db68a0cae
TheNovi <jakub.novi.novacek@gmail.com>
2025-09-22 23:41:15 +0200

    Tod Sort Items by name

--- src/lib/server/tod.ts
+++ src/lib/server/tod.ts
@@ -65,7 +65,7 @@ async function getChildren(userId: number, parentId: number | null) {
 		.select({ id: todItem.id, title: todItem.title, state: todItem.state, dtStart: todItem.dtStart, dtEnd: todItem.dtEnd })
 		.from(todItem)
 		.where(basicWhere(userId, parentId ? eq(todItem.parentId, parentId) : isNull(todItem.parentId)))
-		.orderBy(todItem.dtStart, desc(todItem.state), desc(todItem.id));
+		.orderBy(todItem.dtStart, desc(todItem.state), todItem.title, desc(todItem.id));
 	// .toSQL().sql;
 }
 
